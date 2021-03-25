39d68cdc17934470678040b8376fe983175ac9fe
Aaron Hobson <aaron@ahob85.com>
2021-03-25 13:37:42 -0700

    update

--- complete/script.js
+++ complete/script.js
@@ -8,9 +8,6 @@ const CANVAS_HEIGHT = 400;
 
 /*
  * setup()
- * This is a standard p5.js function that is automatically called once. As you * can guess by its name, you should put any "setup code" in this function
- * (e.g., initializing the canvas, global variable values, placing characters
- * sprites onto the canvas, etc).
  */
 function setup() {
   // assign canvas variable to a new canvas of CANVAS_WIDTH, CANVAS_HEIGHT
@@ -20,7 +17,7 @@ function setup() {
   // attach canvas to the #canvas-area div
   canvas.parent("#canvas-area");
   // attach mousePressed() event to canvas. drawSplatter() is the callback
-  canvas.mousePressed(drawEllipse);
+  canvas.mousePressed(drawSplatter);
   // assign the #clear-button button element to the button variable
   button = select("#clear-button");
   // attach mousePressed() event to button. setup() is the callback
@@ -38,9 +35,7 @@ function setup() {
  * This function should draw an ellipse. For the sake of this demo, let's make * it "red" (#ff0000).
  */
  function drawEllipse() {
-   // the next shape we draw will be "red" (#ff0000)
    fill("#ff0000");
-   // draw an ellipse at mouseX, mouseY, width and height both 100
    ellipse(mouseX, mouseY, 100, 100);
  }
 
@@ -50,24 +45,27 @@ function setup() {
  * For example, if the slider is at 75, the spread will be set to 75 as well.
  */
 function updateSpread() {
-  // set spread variable to the value of the slider element
   spread = slider.value();
 }
 
- /*
-  * drawSplatter()
-  * Creates ellipses on the canvas around where the mouse is clicked.
-  * Details:
-  * -The function creates between 10 and 14 ellipses (random).
-  * -Each ellipse will be a random color and have no outline.
-  * -Each ellipse will have a random radius (5 to 14 pixels).
-  * -Each ellipse's x and y values are calculated like this:
-  *  x = random value between mouseX - spread and mouseX + spread
-  *  y = random value between mouseY - spread and mouseY + spread
-  */
-  function drawSplatter(){
-    fill(random(100, 255), random(100, 255), random(100, 255));
-    noStroke();
-    // the magic happens below!
-    
+/*
+ * drawSplatter()
+ * Creates ellipses on the canvas around where the mouse is clicked.
+ * Details:
+ * -The function creates between 10 and 14 ellipses (random).
+ * -Each ellipse will be a random color and have no outline.
+ * -Each ellipse will have a random radius (5 to 14 pixels).
+ * -Each ellipse's x and y values are calculated like this:
+ *  x = random value between mouseX - spread and mouseX + spread
+ *  y = random value between mouseY - spread and mouseY + spread
+ */
+function drawSplatter(){
+  fill(random(100, 255), random(100, 255), random(100, 255));
+  noStroke();
+  // the magic happens below!
+  let randomNum = random(10, 15);
+  for(let i = 0; i < randomNum; i++) {
+   	let randomSize = random(5, 15);
+   	ellipse(random(mouseX - spread, mouseX + spread), random(mouseY - spread, mouseY + spread), randomSize, randomSize);
   }
+}
