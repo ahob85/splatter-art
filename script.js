b26e1d91546c5e83eb380604cc8d5418bdddf70b
Aaron Hobson <aaron@ahob85.com>
2021-03-25 12:46:15 -0700

    update

---
+++ script.js
@@ -0,0 +1,42 @@
+// declare global variables
+let canvas;
+let button;
+let slider;
+let spread;
+const CANVAS_WIDTH = 500;
+const CANVAS_HEIGHT = 400;
+
+/*
+ * setup()
+ * This is a standard p5.js function that is automatically called once. As you * can guess by its name, you should put any "setup code" in this function
+ * (e.g., initializing the canvas, global variable values, placing characters
+ * sprites onto the canvas, etc).
+ */
+function setup() {
+
+}
+
+/*
+ * drawEllipse()
+ * This function should draw an ellipse. For the sake of this demo, let's make * it "red" (#ff0000).
+ */
+
+
+/*
+ * updateSpread()
+ * This function simply sets the spread variable to the value of the slider.
+ * For example, if the slider is at 75, the spread will be set to 75 as well.
+ */
+
+
+ /*
+  * drawSplatter()
+  * Creates ellipses on the canvas around where the mouse is clicked.
+  * Details:
+  * -The function creates between 10 and 14 ellipses (random).
+  * -Each ellipse will be a random color and have no outline.
+  * -Each ellipse will have a random radius (5 to 14 pixels).
+  * -Each ellipse's x and y values are calculated like this:
+  *  x = random value between mouseX - spread and mouseX + spread
+  *  y = random value between mouseY - spread and mouseY + spread
+  */
